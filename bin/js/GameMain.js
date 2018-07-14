0ac5ea1c7284fdbe88b185df0ac3249a701f39c8
程嘉梁 <chengjl16@163.com>
2018-07-14 16:12:26 +0800

    0.3.0

--- bin/js/GameMain.js
+++ bin/js/GameMain.js
@@ -6,24 +6,60 @@ var GameMain = /** @class */ (function () {
         //设置屏幕自动平铺和旋转
         Laya.stage.scaleMode = Laya.Stage.SCALE_EXACTFIT;
         Laya.stage.screenMode = Laya.Stage.SCREEN_HORIZONTAL;
+        //初始化界面管理器(开始界面为0，游戏界面为1，结束界面为2)
+        GameMain.viewStack = new Laya.ViewStack();
+        GameMain.viewStack.initItems();
+        Laya.stage.addChild(GameMain.viewStack);
         //加载资源
         var resArray = [
             { url: "ui/background/BackGround.jpg", type: Laya.Loader.IMAGE },
+            { url: "ui/background/StartBackGround.png", type: Laya.Loader.IMAGE },
             { url: "res/atlas/ui/blackhole.atlas", type: Laya.Loader.ATLAS },
             { url: "res/atlas/ui/star.atlas", type: Laya.Loader.ATLAS },
             { url: "res/atlas/ui/arrow.atlas", type: Laya.Loader.ATLAS },
             { url: "res/atlas/ui/stone.atlas", type: Laya.Loader.ATLAS },
             { url: "res/atlas/ui/score.atlas", type: Laya.Loader.ATLAS },
+            { url: "res/atlas/ui/button.atlas", type: Laya.Loader.ATLAS }
         ];
         Laya.loader.load(resArray, Laya.Handler.create(this, this.onLoaded)); //当资源加载完毕时，调用onLoaded
     }
-    //加载资源完毕后，游戏的开始界面
+    //加载资源完毕后，创建界面
     GameMain.prototype.onLoaded = function () {
-        //仅用于开发阶段
-        GameMain.gameView = new GameView(); //将游戏主界面作为游戏的开始界面
-        Laya.stage.addChild(GameMain.gameView);
+        GameMain.gameView = new GameView();
+        GameMain.startView = new ui.StartViewUI();
+        GameMain.viewStack.addItem(GameMain.startView);
+        GameMain.viewStack.addItem(GameMain.gameView);
+        this.createEvents();
+    };
+    //创建各种响应事件
+    GameMain.prototype.createEvents = function () {
+        //开始界面的开始按钮
+        GameMain.startView.startButtonView.on(Laya.Event.MOUSE_MOVE, this, function () {
+            GameMain.startView.startButtonView.scale(1.1, 1.1);
+        });
+        GameMain.startView.startButtonView.on(Laya.Event.MOUSE_OUT, this, function () {
+            GameMain.startView.startButtonView.scale(1, 1);
+        });
+        GameMain.startView.startButtonView.on(Laya.Event.CLICK, this, this.toGameView);
+        //开始界面的排行榜按钮
+        GameMain.startView.rankButtonView.on(Laya.Event.MOUSE_MOVE, this, function () {
+            GameMain.startView.rankButtonView.scale(1.1, 1.1);
+        });
+        GameMain.startView.rankButtonView.on(Laya.Event.MOUSE_OUT, this, function () {
+            GameMain.startView.rankButtonView.scale(1, 1);
+        });
+        //开始界面的存储按钮
+        GameMain.startView.saveButtonView.on(Laya.Event.MOUSE_MOVE, this, function () {
+            GameMain.startView.saveButtonView.scale(1.1, 1.1);
+        });
+        GameMain.startView.saveButtonView.on(Laya.Event.MOUSE_OUT, this, function () {
+            GameMain.startView.saveButtonView.scale(1, 1);
+        });
+    };
+    //到游戏界面
+    GameMain.prototype.toGameView = function () {
+        GameMain.viewStack.selectedIndex = 1;
         GameMain.gameView.gameStart(); //开始游戏
-        //仅用于开发阶段
     };
     return GameMain;
 }());
